1e3561cdc12fffa04e47a257f01119d3b1aaa94a
paasch <sebastian.paasch@neofonie.de>
2016-06-09 12:46:09 +0200

    init

---
+++ url-mapping/src/main/java/de/neofonie/surlgen/urlmapping/UrlMappingFilter.java
@@ -0,0 +1,70 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Neofonie GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package de.neofonie.surlgen.urlmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class UrlMappingFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(UrlMappingFilter.class);
+
+    private UrlMappingService urlMappingService;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        if (!(request instanceof HttpServletRequest)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        HttpServletRequest req = (HttpServletRequest) request;
+        final String mapping = urlMappingService.getMapping(req.getRequestURI());
+        if (mapping == null) {
+            chain.doFilter(request, response);
+            return;
+        }
+        request.getRequestDispatcher(mapping).include(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+    @Autowired
+    public void setUrlMappingService(UrlMappingService urlMappingService) {
+        this.urlMappingService = urlMappingService;
+    }
+}
---
+++ url-mapping/src/test/java/de/neofonie/surlgen/urlmapping/UrlMappingFilterTest.java
@@ -0,0 +1,76 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Neofonie GmbH
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package de.neofonie.surlgen.urlmapping;
+
+import org.easymock.EasyMock;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import javax.servlet.FilterChain;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class UrlMappingFilterTest {
+
+    private final UrlMappingFilter urlMappingFilter = new UrlMappingFilter();
+    private final UrlMappingService urlMappingService = EasyMock.createMock(UrlMappingService.class);
+    private final ServletResponse response = new MockHttpServletResponse();
+    private final FilterChain filterChain = EasyMock.createMock(FilterChain.class);
+    private final HttpServletRequest httpServletRequest = EasyMock.createMock(HttpServletRequest.class);
+
+    @Before
+    public void setUp() throws Exception {
+        urlMappingFilter.setUrlMappingService(urlMappingService);
+        EasyMock.reset(urlMappingService, filterChain, httpServletRequest);
+    }
+
+    @Test
+    public void testDoFilter_NoMapping() throws Exception {
+        EasyMock.expect(httpServletRequest.getRequestURI()).andReturn("/requestUri");
+
+        filterChain.doFilter(httpServletRequest, response);
+        EasyMock.expect(urlMappingService.getMapping("/requestUri")).andReturn(null);
+
+        EasyMock.replay(urlMappingService, filterChain, httpServletRequest);
+        urlMappingFilter.doFilter(httpServletRequest, response, filterChain);
+        EasyMock.verify(urlMappingService, filterChain, httpServletRequest);
+    }
+
+    @Test
+    public void testDoFilter_Mapping() throws Exception {
+
+        EasyMock.expect(httpServletRequest.getRequestURI()).andReturn("/requestUri");
+        RequestDispatcher requestDispatcher = EasyMock.createMock(RequestDispatcher.class);
+        EasyMock.expect(urlMappingService.getMapping("/requestUri")).andReturn("/newMapping");
+        EasyMock.expect(httpServletRequest.getRequestDispatcher("/newMapping")).andReturn(requestDispatcher);
+        requestDispatcher.include(httpServletRequest, response);
+
+        EasyMock.replay(urlMappingService, filterChain, httpServletRequest, requestDispatcher);
+        urlMappingFilter.doFilter(httpServletRequest, response, filterChain);
+        EasyMock.verify(urlMappingService, filterChain, httpServletRequest, requestDispatcher);
+    }
+}
